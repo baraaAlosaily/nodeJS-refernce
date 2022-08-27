c6b8e0c41d846bea343f7ecda10625e78bcebc04
baraaAlosaily <seelbaraa@gmail.com>
2022-08-27 23:02:48 +0300

    done

--- http-server/index.js
+++ http-server/index.js
@@ -1,15 +1,45 @@
-const http=require('http')
+const http=require('http');
 
 const PORT=3000;
 
-const server= http.createServer((req,res)=>{
-    res.writeHead(200,{
-        'Content-Type':'application/json',
-    });
-    res.end(JSON.stringify({
-        id:1,
-        name:'Sir Issac Newton'
-    }));
+const server= http.createServer();
+
+const friends=[
+        {
+            id:0,
+            name:'Sir Issac Newton'
+        },
+        {
+            id:1,
+            name:'Sir Necola Tesla'
+        }
+]
+
+server.on('request',(req,res)=>{
+    const items=req.url.split('/');
+    if(items[1]==='friends'){
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        if(items.length===3){
+            res.end(JSON.stringify(friends.filter((ele)=>ele.id===parseInt(items[2]))));
+        }else{
+            res.end(JSON.stringify(friends));
+        }
+    }else if(items[1]==='messages'){
+        res.setHeader('Content-type','text/html')
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<ul>')
+        res.write('<li>Hello Isaac!</li>')
+        res.write('<li>What are your thoughts on astronomy?</li>')
+        res.write('</ul>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end()
+    }else{
+        statusCode=404
+        res.end();
+    }
 })
 
 server.listen(PORT,()=>{
